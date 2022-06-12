1b478151522cdd6b09afac7f66ce91fd86982acf
Piotr <piotr.bania@europe.com>
2022-06-12 15:28:56 +0100

    init

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Header 1</h1>
+            <h2>Header 2</h2>
+            <h3>Header 3</h3>
+            <p>This is a paragraph.</p>
+        </header>
+    )
+}
+
+export default Header
